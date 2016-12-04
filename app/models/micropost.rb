9134e552d02979dc359758d211e374a2b44ca2c5
Kyle Hersey <kmhersey951@gmail.com>
2016-12-04 18:05:52 -0500

    fixed question 2 requirements

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 140 }, presence: true
+  validates :content, length: { maximum: 125 }, presence: true
+  before_save { |micropost| micropost.content = content.upcase }
 end
